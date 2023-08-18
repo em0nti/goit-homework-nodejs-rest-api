7a46c319ca4ac03452656935e77cd9daad2ddd6d
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-08-18 16:38:44 +0300

    Refactor code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require("./app");
+const { PORT } = require("./helpers/constants");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+app.listen(PORT, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
