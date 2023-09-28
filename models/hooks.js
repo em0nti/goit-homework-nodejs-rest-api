54f1db654665582f4865139676327b8ac3d8966c
em0nti <39101979+em0nti@users.noreply.github.com>
2023-09-28 12:18:23 +0300

    Implement handling of 409 error

--- models/hooks.js
+++ models/hooks.js
@@ -1,5 +1,11 @@
 const handleSchemaError = (error, data, next) => {
-  error.status = 400;
+    const { name, code } = error;
+    if (name === 'MongoServerError' && code === 11000) {
+      error.status = 409;
+      error.message = 'Email already exist';
+    } else {
+      error.status = 400;
+    }
   next();
 };
 
