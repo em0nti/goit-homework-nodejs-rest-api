0dbce8183d83f8b6e4dad8c37555c58ad2bb89ef
em0nti <39101979+em0nti@users.noreply.github.com>
2023-10-01 22:11:06 +0300

    Add Joi schema for user validation

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,10 @@
 const contactSchema = require('./contact-schema');
 const updateFavoriteSchema = require('./favorite-schema');
+const { userRegistrationSchema, userLoginSchema } = require('./user-schema');
 
 module.exports = {
   contactSchema,
   updateFavoriteSchema,
+  userRegistrationSchema,
+  userLoginSchema,
 };
