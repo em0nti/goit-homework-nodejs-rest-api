1410a10ca520590fa21a2f5cf8520a047d2c762f
em0nti <39101979+em0nti@users.noreply.github.com>
2023-10-02 11:00:30 +0300

    Add schema for DB

---
+++ models/user-model.js
@@ -0,0 +1,29 @@
+const { model, Schema } = require('mongoose');
+const { handleSchemaError, runValidateAtUpdate } = require('./hooks');
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ['starter', 'pro', 'business'],
+    default: 'starter',
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+userSchema.post('save', handleSchemaError);
+userSchema.pre('findOneAndUpdate', runValidateAtUpdate);
+userSchema.post('findOneAndUpdate', handleSchemaError);
+
+module.exports = model('user', userSchema);
