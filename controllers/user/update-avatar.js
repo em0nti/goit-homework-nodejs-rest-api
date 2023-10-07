21632a5445ed08e0f16f00b97fd9d658441b446f
em0nti <39101979+em0nti@users.noreply.github.com>
2023-10-07 16:35:33 +0300

    implement controller to update avatar

--- controllers/user/update-avatar.js
+++ controllers/user/update-avatar.js
@@ -1,8 +1,21 @@
 const { ctrlWrapper } = require('../../decorators');
+const sharp = require('sharp');
+const path = require('path');
+const { UserModel } = require('../../models');
+
+const avatarDirPath = path.resolve('public', 'avatars');
 
 const updateAvatar = async (req, res, next) => {
-	console.log(req.file);
-	res.json({ message: 'Avatar updated succesfuly' });
+	const { _id } = req.user;
+	const { path: temporaryFile, originalname } = req.file;
+	const avatarFilename = `${_id}-${originalname}`;
+	const avatarFilenamePath = path.join(avatarDirPath, avatarFilename);
+
+	await sharp(temporaryFile).resize(250, 250).toFile(avatarFilenamePath);
+
+	const avatarUrl = path.join('avatars', avatarFilename);
+	await UserModel.findByIdAndUpdate(_id, { avatarUrl });
+	res.json({ code: 200, avatarUrl });
 };
 
 module.exports = ctrlWrapper(updateAvatar);
